84d24aca75162f09825b9fc72aca4a4fd6251c1a
Leo <Leonardo.Rodriguez92@gmail.com>
2023-08-31 15:56:18 -0400

    add container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Boards from './components/Boards';
 import Nav from './components/Nav';
+import {Routes, Route} from 'react-router-dom';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Container sx={{mt: "85px"}}>
+        <Routes>
+          <Route path="/" element={<Boards/>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
